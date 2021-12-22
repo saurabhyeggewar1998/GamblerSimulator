9734620cb4267e87cddc25405bb63de6d1c2f2d6
saurabhyeggewar1998 <yeggewar@gmail.com>
2021-12-22 09:16:54 +0530

    start with stake

---
+++ GamblerSimulatorUC1.java
@@ -0,0 +1,12 @@
+
+
+public class GamblerSimulatorUC1 {
+public static final int INITIAL_BET_STAKE = 100;
+public static final int BET_AMT = 1;
+
+
+	public static void main(String[] args) {
+		
+	}
+
+}
